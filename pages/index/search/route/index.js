80f3a6e856d2823dd9b0c17b5136d9b4ebda0fa1
episode <episode.moe@gmail.com>
2024-09-06 22:56:34 +0800

    完成搜索功能

--- pages/index/search/route/index.js
+++ pages/index/search/route/index.js
@@ -1,11 +1,22 @@
-// pages/index/search/route/index.js
+import themeChangeBehavior from 'tdesign-miniprogram/mixins/theme-change';
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    behaviors: [themeChangeBehavior],
+    current: 1,
+    autoplay: true,
+    duration: 500,
+    interval: 5000,
+    swiperList: [], // Initialize as an empty array
+    item: {
+      images: [] // Initialize with an empty array
+    },
+    visible: false,   // 控制弹出层的显示/隐藏
+    popupText: '',    // 弹出层显示的内容
   },
 
   /**
@@ -17,11 +28,48 @@ Page({
     if (currentItem) {
       this.setData({
         item:currentItem
+      }, () => {
+        // After setting item, generate swiperList
+        this.updateSwiperList();
       });
     }
 
     console.log(this.data.item);
   },
+
+  updateSwiperList() {
+    const swiperList = this.data.item.images.map((image, index) => ({
+      value: image,
+      ariaLabel: `图片${index + 1}`, // Label based on index
+    }));
+
+    this.setData({
+      swiperList
+    });
+  },
+
+  
+    // 控制单元格点击后弹出层的逻辑
+    handleCellPopup() {
+      const itinerary = this.data.item.itinerary;
+
+      this.setData(
+        {
+          popupText: itinerary,
+        },
+        () => {
+          this.setData({ visible: true });
+        },
+      );
+    },
+    
+    // 弹出层关闭时的事件处理
+    onVisibleChange(e) {
+      this.setData({
+        visible: e.detail.visible,
+      });
+    },
+  
 
   /**
    * 生命周期函数--监听页面初次渲染完成
