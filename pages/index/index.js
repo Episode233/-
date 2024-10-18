783e836474b017385c58ff3b0d0a1d96b001ae89
episode <episode.moe@gmail.com>
2024-10-18 16:06:12 +0800

    首页更新

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,18 +1,29 @@
 // index.js
 import themeChangeBehavior from 'tdesign-miniprogram/mixins/theme-change';
-const imageCdn = 'https://tdesign.gtimg.com/mobile/demos';
 const swiperList = [
-  `${imageCdn}/swiper1.png`,
-  `${imageCdn}/swiper2.png`,
-  `${imageCdn}/swiper1.png`,
-  `${imageCdn}/swiper2.png`,
-  `${imageCdn}/swiper1.png`,
+  `https://r2.episode.moe/lskypro/2024/10/18/67121614148bd.png`,
+  `https://r2.episode.moe/lskypro/2024/10/18/6712161b9d694.png`,
+  `https://r2.episode.moe/lskypro/2024/10/13/670aa13d53e87.png`,
+  `https://r2.episode.moe/lskypro/2024/10/18/67121620a918d.jpg`,
+  `https://r2.episode.moe/lskypro/2024/10/18/67121621094b4.jpg`,
 ];
 Page({
   behaviors: [themeChangeBehavior],
 
   navToSearchPage() {
     wx.navigateTo({ url: '/pages/index/search/index' });
+  },
+
+  handleItemClick(e) {
+    const item = e.currentTarget.dataset.item;
+    
+    // 将数据存储到本地缓存
+    wx.setStorageSync('currentItem', item);
+    
+    // 跳转到目标页面
+    wx.navigateTo({
+      url: '/pages/index/search/route/index'
+    });
   },
   data:{
     current: 2,
@@ -34,6 +45,29 @@ Page({
     backTopText: '顶部',
     tabPanelstyle: 'display:flex;justify-content:center;align-items:center;',
   },
+  onLoad(options){
+    wx.request({
+      url: `https://api_hzkj.episode.ink/route/?destination=`, // 动态传递 value
+      method: 'GET',
+      success: (res) => {
+        // 假设返回的数据格式为 { data: ['tdesign-vue', 'tdesign-react', ...] }
+        const list = res.data; 
+
+        this.setData({
+          resultList: list.map(item => item),
+        });
+
+        console.log("resultList:",this.data.resultList);
+      },
+      fail: (err) => {
+        console.error('请求失败', err);
+        this.setData({
+          resultList: [] // 请求失败时清空列表
+        });
+      }
+    });
+  },
+
   onTabsChange(event) {
     console.log(`Change tab, tab-panel value is ${event.detail.value}.`);
   },
