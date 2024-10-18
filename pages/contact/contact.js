b9f72aec38fd7af6fe2abb75a55ca9008552ef9d
episode <episode.moe@gmail.com>
2024-10-18 16:43:24 +0800

    项目整合

--- pages/contact/contact.js
+++ pages/contact/contact.js
@@ -1,68 +1,50 @@
 // pages/contact/contact.js
+const swiperList = [
+  `https://r2.episode.moe/lskypro/2024/10/13/670abe8d9cd41.png`,
+  `https://r2.episode.moe/lskypro/2024/10/18/6712161213faf.jpg`,
+  `https://r2.episode.moe/lskypro/2024/10/18/67121642c26ff.png`,
+  `https://r2.episode.moe/lskypro/2024/10/18/671216861f8cc.jpg`,
+  `https://r2.episode.moe/lskypro/2024/10/13/670aa5d4633b7.png`,
+];
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
-  },
-  methods: {
-   
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad(options) {
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
+    current: 2,
+    autoplay: true,
+    duration: 500,
+    interval: 5000,
+    paginationPosition: 'bottom-right',
+    swiperList,
+    navigation: { type: 'fraction' },
+    visible: false, // 控制弹出层的显示
+    cur: {}, // 存储弹出层的配置
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
+  // 点击“联系我们”时触发的函数
+  onContactTap() {
+    this.setData({
+      visible: true,
+      cur: {
+        value: 'center', // 弹出层的位置
+        text: '客服热线：400-696-5181', // 弹出层显示的内容
+      },
+    });
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
+  // 点击“关于我们”时触发的函数
+  onAboutTap() {
+    this.setData({
+      visible: true,
+      cur: {
+        value: 'bottom',
+        text: '北京瀚竺科技有限公司是一家专注于为在职职工提供身心休养、为退休职工提供健康疗养、为所有社会有需求人士提供个性化定制行程的服务型公司；公司不仅拥有一支专业的金牌客服团队，同时在全国20多个省份拥有近北京瀚竺科技有限公司百家长期合作的优质康养基地。公司本着“服务至上、精益求精”的理念，不断推进高品质身心休养服务的技术创新和模式优化，为客户提供个性化的一站式综合身心休养定制服务',
+      },
+    });
   },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
+  // 处理弹出层的可见性变化
+  onVisibleChange(e) {
+    this.setData({
+      visible: e.detail.visible,
+    });
   },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
-  }
-})
+});
